b3a3333679b247d65dfd1696e6a172ee5612a1a3
Brandon Shearin <brandon.shearin@ibm.com>
2020-12-22 09:15:10 -0500

    clean up list view

--- Cookooree/Views/Aggregate Recipe Views/RecipeListView.swift
+++ Cookooree/Views/Aggregate Recipe Views/RecipeListView.swift
@@ -9,51 +9,29 @@ import SwiftUI
 
 struct RecipeListView: View {
     
-    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"].map { $0.uppercased()}
-    
     var recipes: [Recipe]
     var sortOrder: String
-    
-    var groupedRecipes: [[Recipe]] {
-        recipes.reduce([[Recipe]]()) {
-            guard var last = $0.last else { return [[$1]] }
-            var collection = $0
-            if last.first!.recipeName.first == $1.recipeName.first {
-                last += [$1]
-                collection[collection.count - 1] = last
-            } else {
-                collection += [[$1]]
-            }
-            return collection
-        }
-    }
     
     @EnvironmentObject var dataController: DataController
     
     var body: some View {
         ScrollView {
-                ForEach(groupedRecipes, id: \.self) { group in
-                    Group {
-                        ForEach(group) { recipe in
-                            NavigationLink(destination: RecipeDetailsView(recipe: recipe)){
-                                RecipeRowView(recipe: recipe)
-                            }
-                        }
-                        .onDelete { offsets in
-                            let allRecipes = group
-                            for offest in offsets {
-                                let recipe = allRecipes[offest]
-                                dataController.delete(recipe)
-                            }
-            
-                            dataController.save()
-                        }
-                    }
-                    .padding(.horizontal)
-                    .id(group.first?.recipeName.prefix(1))
-            
+            ForEach(recipes) { recipe in
+                NavigationLink(destination: RecipeDetailsView(recipe: recipe)){
+                    RecipeRowView(recipe: recipe)
                 }
             }
+            .onDelete { offsets in
+                let allRecipes = recipes
+                for offest in offsets {
+                    let recipe = allRecipes[offest]
+                    dataController.delete(recipe)
+                }
+                
+                dataController.save()
+            }
+            .padding(.horizontal)
+        }
         
     }
 }
